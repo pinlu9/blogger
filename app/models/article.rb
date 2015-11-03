781d5f9a4d307ec565bf5ea924c93e82af8b3b98
Pin <pin@Lu-MacPro.local>
2015-11-02 20:25:11 -0500

    updated

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,10 @@ class Article < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings
 
+  # has_attached_file :image
+  # validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  #
+
 end
 
 def tag_list
